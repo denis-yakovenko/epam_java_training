0bbe5f58e0e00a6f327737c7dc5b1cc9ba720a2e
Denis Yakovenko <maximka1945@gmail.com>
2017-08-03 08:18:31 +0300

    Removed ArrayList. Added catching of StackOverflowError on recursive call to prevent it

--- src/fibonacci.java
+++ src/fibonacci.java
@@ -1,5 +1,4 @@
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -8,46 +7,45 @@ import java.util.Scanner;
 public class fibonacci {
 
     private static Integer maxIterations;
+    private static Integer index2Show;
+    private static BigDecimal number2Show;
     private static Integer evenCount = 0;
     private static Integer oddCount = 0;
-    private static ArrayList<BigDecimal> fibonacciList = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String inputLine;
         System.out.println("input number of fibonacci to get");
         while (true) {
             try {
-                inputLine = scanner.nextLine();
-                maxIterations = Integer.parseInt(inputLine);
+                maxIterations = Integer.parseInt(scanner.nextLine());
                 break;
             } catch (Exception e) {
                 System.out.println("invalid number of maximum iterations. please input valid number");
+            }
+        }
+        System.out.println("input an index of fibonacci array you want to show");
+        while (true) {
+            try {
+                index2Show = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (Exception e) {
+                System.out.println("invalid index of an array of fibonacci list. please input valid number");
             }
         }
         calcFibonacci(0, BigDecimal.ZERO, BigDecimal.ZERO);
         System.out.println(String.format("evens: %d pieces, odds: %d pieces", evenCount, oddCount));
         System.out.println(String.format(
                 "evens: %.2f%%, odds: %.2f%%",
-                (float) evenCount / maxIterations * 100,
-                (float) oddCount / maxIterations * 100));
-        System.out.println("input an index of fibonacci array you want to show");
-        while (true) {
-            try {
-                System.out.println(
-                        fibonacciList.get(Integer.parseInt(scanner.nextLine())
-                        ));
-                break;
-            } catch (Exception e) {
-                System.out.println("invalid index of an array of fibonacci list. please input valid number");
-            }
-        }
+                (float) evenCount / (evenCount + oddCount) * 100,
+                (float) oddCount / (evenCount + oddCount) * 100));
+        System.out.println(String.format("number with index %d is %s", index2Show, number2Show));
     }
 
     private static void calcFibonacci(int iteration, BigDecimal currentSum, BigDecimal previousSum) {
         BigDecimal newCurrentSum;
         System.out.println(String.format("iteration: %d value: %s", iteration, currentSum));
-        fibonacciList.add(currentSum);
+        if (iteration == index2Show)
+            number2Show = currentSum;
         if (currentSum.remainder(BigDecimal.valueOf(2)).equals(BigDecimal.valueOf(0)))
             evenCount++;
         else oddCount++;
@@ -59,7 +57,12 @@ public class fibonacci {
         }
         previousSum = currentSum;
         if (iteration < maxIterations)
-            calcFibonacci(iteration, newCurrentSum, previousSum);
+            try {
+
+                calcFibonacci(iteration, newCurrentSum, previousSum);
+            } catch (StackOverflowError e) {
+                System.out.println("unable to calculate more numbers");
+            }
     }
 
 }
