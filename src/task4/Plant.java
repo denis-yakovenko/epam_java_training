7f5bcb7c568386645b11f3ac5a4a927588575930
Denys Yakovenko <user@EPUALVIW0353.kyiv.epam.com>
2017-08-10 18:44:04 +0300

    task4 second attempt

--- src/task4/Plant.java
+++ src/task4/Plant.java
@@ -5,12 +5,14 @@ public abstract class Plant implements CanSmell {
     private Color color;
     private Integer height;
     private Freshness freshness;
+    private Integer price;
 
-    public Plant(String name, Color color, Integer height, Freshness freshness) {
+    public Plant(String name, Color color, Integer height, Freshness freshness, Integer price) {
         this.name = name;
         this.color = color;
         this.height = height;
         this.freshness = freshness;
+        this.price = price;
     }
 
     public String getName() {
@@ -29,6 +31,10 @@ public abstract class Plant implements CanSmell {
         return freshness;
     }
 
+    public Integer getPrice() {
+        return price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -37,7 +43,8 @@ public abstract class Plant implements CanSmell {
         return getName().equals(plant.getName()) &&
                 getColor() == plant.getColor() &&
                 getHeight().equals(plant.getHeight()) &&
-                getFreshness().equals(plant.getFreshness());
+                getFreshness().equals(plant.getFreshness()) &&
+                getPrice().equals(plant.getPrice());
 
     }
 
@@ -47,6 +54,18 @@ public abstract class Plant implements CanSmell {
         result = 31 * result + getColor().hashCode();
         result = 31 * result + getHeight().hashCode();
         result = 31 * result + getFreshness().hashCode();
+        result = 31 * result + getPrice().hashCode();
         return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Plant{" +
+                "name='" + name + '\'' +
+                ", color=" + color +
+                ", height=" + height +
+                ", freshness=" + freshness +
+                ", price=" + price +
+                '}';
     }
 }
